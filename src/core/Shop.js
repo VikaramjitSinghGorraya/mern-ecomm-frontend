bb177b972953d4b05d7cc98169886fbd8607bf7f
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-01-10 22:20:49 -0700

    Fixed the padding of search reasult in shop

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -218,6 +218,7 @@ const SearchResults = styled.div`
         display: flex;
         flex-direction: column;
         justify-content: center;
+        align-items: center;
         /* padding-right: 1.25rem;  */
     }
     /* padding: 1.25rem; */
